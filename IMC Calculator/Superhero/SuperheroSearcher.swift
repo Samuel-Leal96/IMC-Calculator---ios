08f9ae08525fc7ffe1794168fcff22b4e4262e8b
Samuel Leal Vega <saam_lv96@hotmail.com>
2026-06-24 13:46:19 -0600

    Super hero App Init with Textfield finder super hero

---
+++ IMC Calculator/Superhero/SuperheroSearcher.swift
@@ -0,0 +1,18 @@
+//
+//  SuperheroSearcher.swift
+//  IMC Calculator
+//
+//  Created by Samuel Leal Vega on 24/06/26.
+//
+
+import SwiftUI
+
+struct SuperheroSearcher: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuperheroSearcher()
+}
---
+++ New Group/IMC Calculator/IMC_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMC_CalculatorApp.swift
+//  IMC Calculator
+//
+//  Created by Samuel Leal Vega on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct IMC_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
